b98c3c1f2d282737897029da54fdda8b5161f47a
Belyaev Egor <erobel0@mail.ru>
2023-02-11 17:40:06 +0300

    Скрыл офервлоу-у

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+const arr = [1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0]
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <div className={'overfolw'}>
+      {arr.map((el) =>
+        <span className='div'>{el}</span>
+      )}
+      </div>
     </div>
   );
 }
